7604480007ccd33f01843a9df7d6b48201bab809
Vladimir Volek <volek.vladimir@gmail.com>
2021-06-21 12:24:28 +0200

    fix: user agent

--- src/utils/blockfrostAPI.ts
+++ src/utils/blockfrostAPI.ts
@@ -1,8 +1,9 @@
 import { BlockFrostAPI } from '@blockfrost/blockfrost-js';
+import packageJson from '../../package.json';
 
 const api = new BlockFrostAPI({
   projectId: 'v4yhZTgZRjJJbLAPMOmMGtZFg3MGIWiT', //process.env.PROJECT_ID,
-  userAgent: 'yoroi-bridge',
+  userAgent: `${packageJson.name}@${packageJson.version}`,
 });
 
 export { api };
